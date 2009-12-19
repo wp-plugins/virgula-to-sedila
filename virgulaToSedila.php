9056468b53829811cc4f4a559dfc8f6056f4a909
radubilei <radubilei@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-19 18:35:25 +0000

    cleanup
    git-svn-id: https://plugins.svn.wordpress.org/virgula-to-sedila/trunk@184925 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- virgulaToSedila.php
+++ virgulaToSedila.php
@@ -3,7 +3,7 @@
 Plugin Name: Virgulă To Sedilă
 Plugin URI: http://wordpress.org/extend/plugins/virgula-to-sedila/
 Description: Înlocuieşte diacriticele "corecte", cu virgulă, cu cele "incorecte", cu sedilă, dar care se afişează corespunzător şi în Windows XP. Inspirat de plugin-ul lui John Kilroy, XHTML to HTML - http://www.kilroyjames.co.uk/2008/07/xhtml-to-html-wordpress-plugin/
-Version: 0.1
+Version: 0.1.1
 Author: Radu Bilei
 Author URI: http://twitter.com/radubilei/
 */
@@ -17,7 +17,7 @@ function virgulaToSedila($buffer) {
 	$s[0] = 'Ş'; // (S cu sedilă) latin capital letter s with cedilla
 	$s[1] = 'ş'; // (s cu sedilă) latin small letter s with cedilla
 	$s[2] = 'Ţ'; // (T cu sedilă) latin capital letter t with cedilla
-	$s[3] = 'ţ'; // (t cu sedilă) latin small letter t with cedilla	
+	$s[3] = 'ţ'; // (t cu sedilă) latin small letter t with cedilla
 	
   return preg_replace($v, $s, $buffer);
 }
